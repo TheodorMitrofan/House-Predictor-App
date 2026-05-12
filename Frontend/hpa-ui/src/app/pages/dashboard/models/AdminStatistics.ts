7a34837504bc4c1914ab359494694a5ae7ac4712
francesco481 <francescomartinut18@gmail.com>
2026-05-12 23:51:04 +0300

    crud + user dashboard

--- Frontend/hpa-ui/src/app/pages/dashboard/models/AdminStatistics.ts
+++ Frontend/hpa-ui/src/app/pages/dashboard/models/AdminStatistics.ts
@@ -3,4 +3,10 @@ export interface AdminStatistics {
   number_of_admins: number | null;
   number_of_active: number | null;
   new_users_this_month: number | null;
+  total_predictions: number | null;
+  new_predictions_this_month: number | null;
+  dataset_size: number | null;
+  model_accuracy: number | null;
+  model_version: string | null;
+  last_trained_date: string | null;
 }
